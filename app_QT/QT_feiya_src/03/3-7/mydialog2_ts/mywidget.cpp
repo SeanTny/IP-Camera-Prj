17d47ed06a27c3b72282f0c9aa8800d828af3ea8
SeanTny <412117838@qq.com>
2017-08-08 18:44:08 +0800

    添加两个对话框

--- app_QT/QT_feiya_src/03/3-7/mydialog2_ts/mywidget.cpp
+++ app_QT/QT_feiya_src/03/3-7/mydialog2_ts/mywidget.cpp
@@ -3,6 +3,10 @@
 #include "QColor"
 #include "QColorDialog"
 #include "QDebug"
+#include "QFileDialog"
+#include "QFontDialog"
+
+
 
 myWidget::myWidget(QWidget *parent) :
     QWidget(parent),
@@ -21,3 +25,35 @@ void myWidget::on_pushButton_clicked()
     QColor color=QColorDialog::getColor(Qt::red,this,tr("颜色对话框"));
     qDebug()<<"color:"<<color;
 }
+
+
+
+void myWidget::on_pushButton_2_clicked()
+{
+    QStringList fileNames=QFileDialog::getOpenFileNames(this,
+                                                        tr("文件对话框"),
+                                                        "D:",
+                                                        tr("图片文件(*png *jpg);;文本文件(*.txt)")
+                                                        );
+    qDebug()<<"fileNames:"<<fileNames;
+
+}
+
+
+//字体对话框
+void myWidget::on_pushButton_3_clicked()
+{
+    bool ok;
+    QFont font=QFontDialog::getFont(&ok,this);
+    if(ok)
+    {
+        ui->pushButton_3->setFont(font);
+    }
+    else
+    {
+        qDebug()<<"没有选择字体";
+    }
+}
+
+
+
