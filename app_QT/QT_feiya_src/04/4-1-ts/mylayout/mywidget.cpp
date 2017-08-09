fd1e7a1097a855b1a5475cb585fcdab24b89f9bd
SeanTny <412117838@qq.com>
2017-08-10 00:02:28 +0800

    增加窗口管理器4-1-ts的学习文件

--- app_QT/QT_feiya_src/04/4-1-ts/mylayout/mywidget.cpp
+++ app_QT/QT_feiya_src/04/4-1-ts/mylayout/mywidget.cpp
@@ -1,6 +1,7 @@
 #include "mywidget.h"
 #include "ui_mywidget.h"
 #include <QHBoxLayout>
+#include <QGridLayout>
 
 
 
@@ -10,13 +11,25 @@ MyWidget::MyWidget(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QHBoxLayout *layout =new QHBoxLayout; //新建水平布局管理器
-    layout->addWidget(ui->fontComboBox);
-    layout->addWidget(ui->textEdit);
-    layout->setSpacing(50);
-    layout->setContentsMargins(0,0,50,100); // 这是布局管理器到桌面widget的距离  左 上 右 下
+//    QHBoxLayout *layout =new QHBoxLayout; //新建水平布局管理器
+//    layout->addWidget(ui->fontComboBox);
+//    layout->addWidget(ui->textEdit);
+//    layout->setSpacing(50);
+//    layout->setContentsMargins(0,0,50,100); // 这是布局管理器到桌面widget的距离  左 上 右 下
 
-    setLayout(layout);
+//    setLayout(layout);
+
+
+//    QGridLayout *layout = new QGridLayout;
+//    //添加部件，从0行0列开始 占据1行2列
+//    layout->addWidget(ui->fontComboBox,0,0,1,2);
+//    layout->addWidget(ui->pushButton,0,2,1,1);
+//    layout->addWidget(ui->textEdit,1,0,1,3);
+
+//    setLayout(layout);
+
+
+
 
 }
 
